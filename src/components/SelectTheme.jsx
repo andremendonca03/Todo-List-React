32e4388bba4c84bee33b1f65677e8a724cc945c5
Andre Mendonca <andremendonca.033@gmail.com>
2022-08-09 07:44:18 +1000

    SelectTheme Component

---
+++ src/components/SelectTheme.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SelectTheme = () => {
+  return (
+    <span className="todo-theme">
+      Theme:{" "}
+      <select name="" id="">
+        <option value="standart">Standard</option>
+      </select>
+    </span>
+  )
+}
+
+export default SelectTheme;
