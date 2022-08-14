1cf0b09e21e26f44cf0274f781ec769c7c295944
Andre Mendonca <andremendonca.033@gmail.com>
2022-08-14 21:39:49 +1000

    TodoList Component

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { GlobalContext } from "../GlobalContext";
+
+const TodoList = () => {
+  const global = React.useContext(GlobalContext);
+
+  function handleCheck({ target }) {
+    const actualItem = target.parentElement;
+    const checked = () => {
+      if (actualItem.getAttribute("data-checked") === "true") {
+        return true;
+      } else {
+        return false;
+      }
+    };
+
+    global.setTodoItems((prev) => {
+      prev.forEach((item) => {
+        if (item.id === Number.parseInt(actualItem.getAttribute("id"))) {
+          item.checked = !checked();
+        }
+      });
+
+      return [...global.todoItems];
+    });
+  }
+
+  function handleDelete({ target }) {
+    const actualItem = target.parentElement;
+    
+    global.setTodoItems((prev) => {
+      const arrayReduced = prev.reduce((acc, item) => {
+        if (item.id !== Number.parseInt(actualItem.getAttribute("id"))) {
+          return acc.concat([item]);
+        } else {
+          return acc;
+        }
+      }, []);
+      console.log(arrayReduced);
+      return [...arrayReduced];
+    });
+  }
+
+  return (
+    <ul className="list">
+      {global.todoItems.map((item) => {
+        return (
+          <li
+            key={item.id}
+            id={item.id}
+            className="list-item"
+            data-checked={item.checked}
+          >
+            <span className="list-item-detail"></span>
+            <span className="list-item-check" onClick={handleCheck}></span>
+            <p className="list-item-text">{item.text}</p>
+            <span className="list-item-delete" onClick={handleDelete}></span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default TodoList;
