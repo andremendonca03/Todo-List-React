3b325bdb61c01bbfde5a9ce691935e2036637bfe
Andre Mendonca <andremendonca.033@gmail.com>
2022-08-23 12:39:16 +1000

    SelectTheme Component

--- src/components/SelectTheme.jsx
+++ src/components/SelectTheme.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React from "react";
+import { GlobalContext } from "../GlobalContext";
 
 const SelectTheme = () => {
+  const global = React.useContext(GlobalContext);
+
+  function handleTheme({ target }) {
+    global.setTheme(target.value);
+    global.setThemeLocal(target.value);
+  }
+
+  React.useEffect(() => {
+    global.setTheme(global.themeLocal);
+  }, []);
+
+  React.useEffect(() => {
+    document.documentElement.setAttribute('data-theme', `${global.theme}`);
+  }, [global.theme]);
+
   return (
     <span className="todo-theme">
-      Theme:{" "}
-      <select name="" id="">
-        <option value="standart">Standard</option>
+      Theme:
+      <select value={global.theme} onChange={handleTheme} >
+        <option value="standard">Standard</option>
+        <option value="beach">Beach</option>
+        <option value="space">Space</option>
       </select>
     </span>
-  )
-}
+  );
+};
 
 export default SelectTheme;
